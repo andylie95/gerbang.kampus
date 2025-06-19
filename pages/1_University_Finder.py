68d8d02f1baaa4f3b99fc3784a54a3ebe36c12fc
andylie95 <48427509+andylie95@users.noreply.github.com>
2025-06-19 14:10:45 +0700

    Add files via upload

---
+++ pages/1_University_Finder.py
@@ -0,0 +1,13 @@
+IM June Deck: 
+A. Recap
+*Social Media Campaign May&June
+*Webinar Recap
+*Twibon x IM Book Giveaway
+
+B. Ice Breaking Game
+*Registration Number & Applicants Quality
+
+C. What's Next?
+*Timeline - we are here
+*Round 1: Screening Process
+*Round 2: Aptitude Test
